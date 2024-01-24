e21b8509d90490a14179e730ef134fc263d309ec
vishal7997 <40819447+vishal7997@users.noreply.github.com>
2024-01-24 15:17:46 +0530

    Managing states and events in class based components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
-import Demo from './Components/Demo';
-import './App.css';
+import logo from "./logo.svg";
+import Demo from "./Components/Demo";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Demo name={"Hello World!"}>Setting Children props from App component</Demo>
+      <Demo name={"Hello World!"}>
+        Setting Children props from App component
+      </Demo>
     </div>
   );
 }
--- src/Components/Demo.js
+++ src/Components/Demo.js
@@ -1,11 +1,25 @@
 import { Component } from "react";
 
 class Demo extends Component {
+  constructor() {
+    super(); //calling constructor of Component class -> mandatory
+
+    // state initialisation // always assigned with an Object
+    this.state = { showParagraph: true, count: 0 };
+  }
+
+  toggleParagraph() {
+    // console.log(this);
+    this.setState((currState) => {
+      return {showParagraph: !currState.showParagraph};
+    }); // this will not override this.state object, only showParagraph property will be updated here.
+  }
   render() {
     return (
       <>
         <h3>{this.props.name}</h3>
-        <p>{this.props.children}</p>
+        {this.state.showParagraph && <p>{this.props.children}</p>}
+        <button onClick={this.toggleParagraph.bind(this)}>{this.state.showParagraph ? 'Hide' : 'Show'}</button>
       </>
     );
   }
