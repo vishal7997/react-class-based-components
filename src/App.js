7543b78871afe0e0e3f775da05d1f185332f3cc2
vishal7997 <40819447+vishal7997@users.noreply.github.com>
2024-01-24 18:06:37 +0530

    Handling context in class based components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import logo from "./logo.svg";
 import Demo from "./Components/Demo";
 import "./App.css";
+import DemoContext from "./DemoContext";
 
 function App() {
   return (
-    <div className="App">
-      <Demo name={"Hello World!"}>
-        Setting Children props from App component
-      </Demo>
-    </div>
+    <DemoContext.Provider
+      value={{ value: "This value is coming from DemoContext" }}
+    >
+      <div className="App">
+        <Demo name={"Hello World!"}>
+          Setting Children props from App component
+        </Demo>
+      </div>
+    </DemoContext.Provider>
   );
 }
 
--- src/Components/Paragraph.js
+++ src/Components/Paragraph.js
@@ -1,14 +1,28 @@
 import { Component } from "react";
+import DemoContext from "../DemoContext";
 
 class Paragraph extends Component {
+  static contextType = DemoContext; // one way to use context in class based components
   componentWillUnmount() {
     // cleanup function in useEffect
     console.log("componentWillUnmount called");
   }
 
   render() {
-    return <p>This is a Demo paragraph</p>;
+    return <p>{this.context.value}</p>;
   }
+
+  //   render() {
+  //     return (
+  //       <DemoContext.Consumer> // another way to use context in class based components
+  //         {(ctx) => {
+  //             return (
+  //                 <p>{ctx.value}</p>
+  //             )
+  //         }}
+  //       </DemoContext.Consumer>
+  //     );
+  //   }
 }
 
 export default Paragraph;
