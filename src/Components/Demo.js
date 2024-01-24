7b012413ace2eb4284bd9e4435de1548ea4d0c2b
vishal7997 <40819447+vishal7997@users.noreply.github.com>
2024-01-24 17:46:08 +0530

    Handling side effects in class based components - life cycle hooks

--- src/Components/Demo.js
+++ src/Components/Demo.js
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import Paragraph from "./Paragraph";
 
 class Demo extends Component {
   constructor() {
@@ -8,18 +9,36 @@ class Demo extends Component {
     this.state = { showParagraph: true, count: 0 };
   }
 
+  componentDidMount() {
+    // useEffect without any dependencies
+    // LOGIC TO HANDLE SIDE EFFECTS
+    console.log("componentDidMount Called");
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    // useEffect with dependencies
+    // LOGIC TO HANDLE SIDE EFFECTS WHENEVER STATE CHANGES
+    console.log("componentDidUpdate Called");
+    if (prevState.count != this.state.count)
+      this.setState({ count: this.count + 1 });
+  }
+
+
+
   toggleParagraph() {
     // console.log(this);
     this.setState((currState) => {
-      return {showParagraph: !currState.showParagraph};
+      return { showParagraph: !currState.showParagraph };
     }); // this will not override this.state object, only showParagraph property will be updated here.
   }
   render() {
     return (
       <>
         <h3>{this.props.name}</h3>
-        {this.state.showParagraph && <p>{this.props.children}</p>}
-        <button onClick={this.toggleParagraph.bind(this)}>{this.state.showParagraph ? 'Hide' : 'Show'}</button>
+        {this.state.showParagraph && <Paragraph />}
+        <button onClick={this.toggleParagraph.bind(this)}>
+          {this.state.showParagraph ? "Hide" : "Show"}
+        </button>
       </>
     );
   }
